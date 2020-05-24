77dae86f56f5bf04b651120b5e458f304d45ec88
Bala <sbs3895@gmail.com>
2020-05-24 19:57:39 +0530

    Preventing default submit action for html forms

--- js/signin.js
+++ js/signin.js
@@ -1,9 +1,24 @@
 function onLoad_signin() {
      console.log("Inside onLoad_signin");
+     document.getElementById("signinform").addEventListener("submit", function(event){
+               event.preventDefault();
+          });
 }
 function onClickSignin() {
      console.log("Inside onClickSignin");
      var formElements = document.getElementById("signinform");
-     console.log(formElements);
-     alert(JSON.stringify(formElements));
+     var email = formElements.email.value;
+     var psw = formElements.psw.value;
+     var invalid =document.getElementById("invalid");
+
+     console.log(email);
+     console.log(psw);
+     if(email == "email" && psw == "psw"){
+          console.log("valid");
+          invalid.hidden = true;
+          window.location = "shoppingHome.html";
+     }
+     else {
+          invalid.hidden = false;
+     }
 }
